54119ebfbabe73abbbd082f2661c51b00109ed6e
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-04-04 19:07:53 +0530

    Project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "../components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+      <Button variant="default" className="bg-[#121212] hover:bg-[#212121]">click me Satoshi </Button>
+    </>
+  );
+}
