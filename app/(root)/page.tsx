fa6f5ab0d97796ec78151ff375c26abc0f90a27f
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-04-15 12:36:19 +0530

    Top picks section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -78,13 +78,93 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-center gap-5 bg-[#282c32] text-white p-4 m-4 lg:m-12  xl:m-28 rounded-xl">
-          <h1 className="w-[380px] text-3xl text-start">Why Us?</h1>
-          <p className="text-[rgba(255,255,255,0.75)] w-full sm:w-[780px]">At Novice, we're more than just a platform for learning to code. We're your trusted companion
-            on your coding journey, offering expert guidance, a supportive community, and a wealth of resources
-            tailored specifically for novices. Join us and embark on your path to coding mastery with confidence.</p>
-        </div>
+      </section>
+      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-center gap-5 bg-[#282c32] text-white p-4 m-4 lg:m-12  xl:m-28 rounded-xl">
+        <h1 className="w-[380px] text-3xl text-start">Why Us?</h1>
+        <p className="text-[rgba(255,255,255,0.75)] w-full sm:w-[780px]">At Novice, we're more than just a platform for learning to code. We're your trusted companion
+          on your coding journey, offering expert guidance, a supportive community, and a wealth of resources
+          tailored specifically for novices. Join us and embark on your path to coding mastery with confidence.</p>
+      </div>
+      <section className="bg-[#f0f0f0] py-5 md:py-10">
+        <div className="wrapper flex flex-col gap-2">
 
+          <div className="w-full border-b"></div>
+          <p className="font-[500] text-sm">Students' Top Picks</p>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="bg-[#fcfcfc] p-2 rounded-2xl hover:bg-[#fff] transition-all flex flex-col gap-2">
+              {/* Image */}
+              <Image
+                className="w-full h-[200px] bg-[#f0f0f0] rounded-xl"
+                src='./assets/icons/star.svg'
+                width={20}
+                height={20}
+                alt='signup'
+              />
+              <h1 className="text-[#282C32] font-[500] text-base">Frontend Developement</h1>
+              <p className="text-[#282C32] font-[400] text-sm leading-[1.45] tracking-[-0.02px]  ">Titan Crypto, providing exposure to professionally managed crypto ETFs, is now eligible for Roth and Traditional IRAs.</p>
+              <Link href="/">
+                <Button className='font-[400] gap-2 w-[80px] border hover:border-[#d9d9d9] rounded-lg p-0 hover:bg-white bg-[#fcfcfc] text-[#282C32] text-md flex items-center justify-center cursor-pointer transition-all'>
+                  View
+                  <Image
+                    className=" stroke-none"
+                    src='./assets/icons/arrow-square-rightDark.svg'
+                    width={20}
+                    height={20}
+                    alt='signup'
+                  />
+                </Button>
+              </Link>
+            </div>
+            <div className="bg-[#fcfcfc] p-2 rounded-2xl hover:bg-[#fff] transition-all flex flex-col gap-2">
+              {/* Image */}
+              <Image
+                className="w-full h-[200px] bg-[#f0f0f0] rounded-xl"
+                src='./assets/icons/star.svg'
+                width={20}
+                height={20}
+                alt='signup'
+              />
+              <h1 className="text-[#282C32] font-[500] text-base">CP</h1>
+              <p className="text-[#282C32] font-[400] text-sm leading-[1.45] tracking-[-0.02px]  ">Titan Crypto, providing exposure to professionally managed crypto ETFs, is now eligible for Roth and Traditional IRAs.</p>
+              <Link href="/">
+                <Button className='font-[400] gap-2 w-[80px] border hover:border-[#d9d9d9] rounded-lg p-0 hover:bg-white bg-[#fcfcfc] text-[#282C32] text-md flex items-center justify-center cursor-pointer transition-all'>
+                  View
+                  <Image
+                    className=" stroke-none"
+                    src='./assets/icons/arrow-square-rightDark.svg'
+                    width={20}
+                    height={20}
+                    alt='signup'
+                  />
+                </Button>
+              </Link>
+            </div>
+            <div className="bg-[#fcfcfc] p-2 rounded-2xl hover:bg-[#fff] transition-all flex flex-col gap-2">
+              {/* Image */}
+              <Image
+                className="w-full h-[200px] bg-[#f0f0f0] rounded-xl"
+                src='./assets/icons/star.svg'
+                width={20}
+                height={20}
+                alt='signup'
+              />
+              <h1 className="text-[#282C32] font-[500] text-base">DSA</h1>
+              <p className="text-[#282C32] font-[400] text-sm leading-[1.45] tracking-[-0.02px]  ">Titan Crypto, providing exposure to professionally managed crypto ETFs, is now eligible for Roth and Traditional IRAs.</p>
+              <Link href="/">
+                <Button className='font-[400] gap-2 w-[80px] border hover:border-[#d9d9d9] rounded-lg p-0 hover:bg-white bg-[#fcfcfc] text-[#282C32] text-md flex items-center justify-center cursor-pointer transition-all'>
+                  View
+                  <Image
+                    className=" stroke-none"
+                    src='./assets/icons/arrow-square-rightDark.svg'
+                    width={20}
+                    height={20}
+                    alt='signup'
+                  />
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </section>
       {/* <Button variant="default" className="bg-[#121212] hover:bg-[#282c32]">click me Satoshi </Button> */}
     </>
