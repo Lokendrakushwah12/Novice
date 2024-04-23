fd61a03e1624c114caa7c249ab843b303a8a34e2
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-04-23 10:52:10 +0530

    create resource

---
+++ app/(root)/resource/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateResource = () => {
+    return (
+        <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+            <h3 className='wrapper h3-bold text-center sm:text-left'>CreateResource</h3>
+        </section>
+    )
+}
+
+export default CreateResource
